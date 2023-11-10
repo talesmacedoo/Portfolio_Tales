f72e85f50cc047d68cce101ba2c5d3c39ad932f2
talesmacedoo <71733219+talesmacedoo@users.noreply.github.com>
2023-11-10 16:38:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () =>{
+    [modal, fade].forEach((el) => el.classList.toggle("hide"))
+
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
 
 function temaMode() {
     const html = document.documentElement
 
     html.classList.toggle('light')
 }
+
